e9e9a3b236585cd8b6cbed698bc00cfd77d07fcc
scof00 <scofprofessional@gmail.com>
2025-08-24 09:23:13 -0400

    Added proper css scaling depending on viewport

--- tauri-app/src/App.jsx
+++ tauri-app/src/App.jsx
@@ -1,5 +1,5 @@
 // App.jsx
-import React from "react";
+import React, { useEffect } from "react";
 import DevPanel from "./Components/DevPanel";
 import { Routes, Route, Outlet } from "react-router-dom";
 import { Home } from "./components/Home";
@@ -8,30 +8,52 @@ import { initDatabase } from "./Utils/databaseInit";
 import { SettingsDisplay } from "./components/Settings";
 import { Scene1 } from "./components/GameFlow/Scene1";
 import { Scene2 } from "./components/GameFlow/Scene2";
+import "./App.css";
 
 export function App() {
   React.useEffect(() => {
     initDatabase();
   }, []);
 
-  return (
-    <Routes>
-      <Route
-      path="/"
-      element={
-        <>
-        <SettingsDisplay/>
-        <Outlet />
-        </>
-      }
-      >
+  useEffect(() => {
+    function scaleGame() {
+      const scaleX = window.innerWidth / 1920;
+      const scaleY = window.innerHeight / 1080;
+      const scale = Math.max(0.3, Math.min(scaleX, scaleY));
 
-      <Route path="/" element={<Home />} />
-      <Route path="/devPanel" element={<DevPanel />} />
-      <Route path="/combat" element={<CombatGeneric />} />
-      <Route path="/Scene1" element={<Scene1 />}/>
-      <Route path="/Scene2" element={<Scene2 />}/>
-      </Route>
-    </Routes>
+      document.documentElement.style.setProperty("--scale", scale);
+
+      // Debug logging
+      console.log("Window size:", window.innerWidth, "x", window.innerHeight);
+      console.log("Scale:", scale);
+      console.log("Final game size:", 1920 * scale, "x", 1080 * scale);
+    }
+
+    window.addEventListener("resize", scaleGame);
+    scaleGame();
+
+    return () => window.removeEventListener("resize", scaleGame);
+  }, []);
+
+  return (
+    <div id="gameContainer">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <SettingsDisplay />
+              <Outlet />
+            </>
+          }
+        >
+          <Route path="/" element={<Home />} />
+          <Route path="/devPanel" element={<DevPanel />} />
+          <Route path="/combat" element={<CombatGeneric />} />
+          <Route path="/Scene1" element={<Scene1 />} />
+          <Route path="/Scene2" element={<Scene2 />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
